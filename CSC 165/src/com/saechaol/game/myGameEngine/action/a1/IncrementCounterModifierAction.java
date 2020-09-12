614bd346b7a8f9553ad5e9f67f6836fd9c568562
saechaol <33850575+saechaol@users.noreply.github.com>
2020-09-12 14:22:08 -0700

    CSC-36: Implemented counter modifier action class

--- CSC 165/src/com/saechaol/game/myGameEngine/action/a1/IncrementCounterModifierAction.java
+++ CSC 165/src/com/saechaol/game/myGameEngine/action/a1/IncrementCounterModifierAction.java
@@ -1,5 +1,36 @@
 package com.saechaol.game.myGameEngine.action.a1;
 
-public class IncrementCounterModifierAction {
+import ray.input.action.AbstractInputAction;
+import ray.rage.game.*;
+import net.java.games.input.Event;
+import com.saechaol.game.a1.MyGame;
 
+/**
+ * A counter modifier action that works with IncrementCounter.java to test the input device
+ * 
+ * @author Lucas
+ */
+public class IncrementCounterModifierAction extends AbstractInputAction {
+
+	private MyGame game;
+	private int incrementAmount = 1;
+	
+	public IncrementCounterModifierAction(MyGame g) {
+		game = g;
+	}
+	
+	@Override
+	public void performAction(float time, Event e) {
+		System.out.println("Modifier action fired. Modifier = " + incrementAmount);
+		incrementAmount++;
+		if (incrementAmount > 5) {
+			System.out.println("Increment Modifier Action setting back to 1");
+			incrementAmount = 1;
+		}
+	}
+	
+	protected int getIncrementAmount() {
+		return incrementAmount;
+	}
+	
 }
