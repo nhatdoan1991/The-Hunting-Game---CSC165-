818182449d68bd269588c36fea48f683829ecafe
saechaol <33850575+saechaol@users.noreply.github.com>
2020-09-12 14:29:46 -0700

    CSC-34: implemented shutdown listener class

--- CSC 165/src/com/saechaol/game/myGameEngine/action/ExitGameAction.java
+++ CSC 165/src/com/saechaol/game/myGameEngine/action/ExitGameAction.java
@@ -1,5 +1,27 @@
 package com.saechaol.game.myGameEngine.action;
 
-public class ExitGameAction {
+/**
+ * A general event listener that will execute game shutdown
+ * 
+ * @author Lucas
+ */
+import ray.input.action.AbstractInputAction;
+import ray.rage.game.*;
+import net.java.games.input.Event;
+import com.saechaol.game.a1.MyGame;
 
+public class ExitGameAction extends AbstractInputAction {
+
+	private MyGame game;
+	
+	public ExitGameAction(MyGame g) {
+		game = g;
+	}
+	
+	@Override
+	public void performAction(float time, Event e) {
+		System.out.println("Shutdown requested");
+		game.setState(Game.State.STOPPING);
+	}
+	
 }
