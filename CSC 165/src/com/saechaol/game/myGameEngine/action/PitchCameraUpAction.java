d9b953ba82744952981e543cf8c7acaf0c22194a
saechaol <33850575+saechaol@users.noreply.github.com>
2020-09-19 20:05:30 -0700

    CSC-10 Implement up/down arrow key movement

--- CSC 165/src/com/saechaol/game/myGameEngine/action/PitchCameraUpAction.java
+++ CSC 165/src/com/saechaol/game/myGameEngine/action/PitchCameraUpAction.java
@@ -1,5 +1,40 @@
 package com.saechaol.game.myGameEngine.action;
 
-public class PitchCameraUpAction {
+import ray.input.action.AbstractInputAction;
+import com.saechaol.game.a1.MyGame;
+import net.java.games.input.Event;
+import ray.rml.Angle;
+import ray.rml.Degreef;
+import ray.rml.Vector3;
+import ray.rml.Vector3f;
+
+public class PitchCameraUpAction extends AbstractInputAction {
+
+	private MyGame game;
+	
+	public PitchCameraUpAction(MyGame g) {
+		game = g;
+	}
+	
+	@Override
+	public void performAction(float time, Event e) {
+		Angle rotationAmount = Degreef.createFrom(1.0f);
+		if (game.camera.getMode() == 'n') {
+			game.dolphinNode.yaw(rotationAmount);
+		} else {
+			System.out.println("Pitch camera up");
+			// UVN Vector is left handed
+			Vector3f uVector = game.camera.getRt();
+			Vector3f vVector = game.camera.getUp();
+			Vector3f nVector = game.camera.getFd();
+			
+			// transform vectors
+			Vector3 vTransform = (vVector.rotate(rotationAmount, uVector)).normalize();
+			Vector3 nTransform = (nVector.rotate(rotationAmount, uVector)).normalize();
+			
+			game.camera.setFd( (Vector3f) nTransform);
+			game.camera.setUp( (Vector3f) vTransform);
+		}
+	}
 
 }
