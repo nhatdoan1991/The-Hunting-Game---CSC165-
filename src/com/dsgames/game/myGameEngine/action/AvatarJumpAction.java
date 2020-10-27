359cc293502e8cd634f55326e2495ac4cc7abdb4
nhatdoan91 <36098431+nhatdoan91@users.noreply.github.com>
2020-10-26 18:26:30 -0700

    csc-118 done set up base game

---
+++ src/com/dsgames/game/myGameEngine/action/AvatarJumpAction.java
@@ -0,0 +1,34 @@
+package com.dsgames.game.myGameEngine.action;
+
+import com.dsgames.game.hunt.MyGame;
+
+import net.java.games.input.Event;
+import ray.input.action.AbstractInputAction;
+
+public class AvatarJumpAction extends AbstractInputAction {
+
+	private MyGame game;
+	private String player;
+	
+	public AvatarJumpAction(MyGame g, String p) {
+		game = g;
+		player = p;
+	}
+	
+	@Override
+	public void performAction(float time, Event e) {
+		switch (player) {
+		case "dolphinEntityOneNode":
+			game.jumpP1 = true;
+			game.dolphinOnePhysicsObject.applyForce(0.0f, 400.0f, 0.0f, 0.0f, 0.0f, 0.0f);
+			game.velocityP1 = 0.0f;
+			break;
+		case "dolphinEntityTwoNode":
+			game.jumpP2 = true;
+			game.dolphinTwoPhysicsObject.applyForce(0.0f, 400.0f, 0.0f, 0.0f, 0.0f, 0.0f);
+			game.velocityP2 = 0.0f;
+			break;
+		}
+	}
+
+}
