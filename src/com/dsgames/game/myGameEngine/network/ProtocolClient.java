323a957b60ed4c036ede9e89026db5920d109a44
saechaol <33850575+saechaol@users.noreply.github.com>
2020-10-29 23:54:37 -0700

    CSC-100 boilerplate network code

--- src/com/dsgames/game/myGameEngine/network/ProtocolClient.java
+++ src/com/dsgames/game/myGameEngine/network/ProtocolClient.java
@@ -1,5 +1,25 @@
 package com.dsgames.game.myGameEngine.network;
 
-public class ProtocolClient {
+import java.io.IOException;
+import java.net.InetAddress;
+import java.util.UUID;
+import java.util.concurrent.ConcurrentHashMap;
 
+import com.dsgames.game.hunt.HuntingGame;
+
+import ray.networking.client.GameConnectionClient;
+
+public class ProtocolClient extends GameConnectionClient {
+
+	public ProtocolClient(InetAddress remoteAddr, int remotePort, InetAddress localAddr, int localPort,
+			ProtocolType protocolType) throws IOException {
+		super(remoteAddr, remotePort, localAddr, localPort, protocolType);
+		// TODO Auto-generated constructor stub
+	}
+	private HuntingGame game;
+	private UUID id;
+	private ConcurrentHashMap<GhostAvatar, Boolean> ghostAvatars;
+	
+	
+	
 }
