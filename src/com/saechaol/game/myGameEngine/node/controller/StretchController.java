b1063d320aa44645888af0b0cf86565e28afe7f3
saechaol <33850575+saechaol@users.noreply.github.com>
2020-11-03 08:23:27 -0800

    Refactored controller

---
+++ src/com/saechaol/game/myGameEngine/node/controller/StretchController.java
@@ -0,0 +1,29 @@
+package com.saechaol.game.myGameEngine.node.Controller;
+
+import ray.rage.scene.Node;
+import ray.rage.scene.controllers.AbstractController;
+import ray.rml.Vector3;
+import ray.rml.Vector3f;
+
+public class StretchController extends AbstractController {
+	   private float growthRate = .003f;
+	   private float cycleTime = 2000.0f;
+	   private float totalTime = 0.0f;
+	   private float direction = 1.0f;
+	   
+	   @Override
+	   protected void updateImpl(float elapsedTimeMillis) {
+		   totalTime += elapsedTimeMillis;
+		   float growthAmount = 1.0f + direction * growthRate;
+		   
+		   if(totalTime > cycleTime) {
+			   direction = -direction;
+			   totalTime = 0.0f;
+		   }
+		   for(Node node : super.controlledNodesList) {
+			   Vector3 nodeScale = node.getLocalScale();
+			   nodeScale = Vector3f.createFrom(nodeScale.x() * growthAmount, nodeScale.y(), nodeScale.z() * growthAmount);
+			   node.setLocalScale(nodeScale);
+		   }
+	   }
+}
